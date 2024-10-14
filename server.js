71a13702fdd65158d990aa6dd821059fbd09af66
SpiderEP <kongkwamsuk_n@silpakorn.edu>
2024-10-14 17:51:18 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('Home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'Home.html'));
+});
+
+
+app.get('About me', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'About me.html'));
+});
+
+
+app.get('Experience', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'Experience.html'));
+});
+
+app.get('Contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'Contact.html'));
+});
+
+app.listen(3000, function () {
+  console.log('Server listening on port 3000');
+});
